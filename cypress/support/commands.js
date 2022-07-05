a2a1651fa8c7070017a0f170a1844105f6a127f7
Vitwon <vitordeaguiar0@gmail.com>
2022-07-05 14:23:09 -0300

    #03 - Logout and Authenticated Commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,11 @@
 
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
-    cy.visit('/signup')
-    cy.get('#email').type(email)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
 })
 
 Cypress.Commands.add('login', (
@@ -69,4 +69,24 @@ Cypress.Commands.add('deleteNote', note => {
   cy.contains('button', 'Delete').click()
 
   cy.contains('.list-group-item', note).should('not.exist')
+})
+
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings')
+  cy.get('#storage').type('1')
+  cy.get('#name').type('Mary Doe')
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242')
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271')
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123')
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345')
+  cy.get('.LoaderButton').click()
 })
