39037d242bbdd65f0c3eff2262b835561cdd3772
Vitwon <vitordeaguiar0@gmail.com>
2022-07-12 17:26:19 -0300

    #13 Error Singup - Commit

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,20 +1,19 @@
 // Ira apresentar falha pois já passou o free test do Mailour Sauor
-// it('successfully signs up using confirmation code sent via email', () => {
-//   const faker = require('faker')
-//   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-//   const password = Cypress.env('USER_PASSWORD')
+it('successfully signs up using confirmation code sent via email', () => {
+  const faker = require('faker')
+  const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+  const password = Cypress.env('USER_PASSWORD')
 
-//   cy.intercept('GET', '**/notes').as('getNotes')
-//   cy.fillSignupFormAndSubmit(emailAddress,password)
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.fillSignupFormAndSubmit(emailAddress,password)
 
-//   cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-//     sentTo: emailAddress
-//   }).then(message => {
-//     const confirmationCode = message.html.body.match(/\d{6}/)[0]
-//     cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: emailAddress
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
 
-//     cy.wait('@getNotes')
-//     cy.contains('h1', 'Your Notes').should('be.visible')
-//   })
-// })
-// test test
+    cy.wait('@getNotes')
+    cy.contains('h1', 'Your Notes').should('be.visible')
+  })
+})
