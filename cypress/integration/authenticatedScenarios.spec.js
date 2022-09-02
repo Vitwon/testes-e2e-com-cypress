b65dd1cea0d084665f4146dced6232f1921cb972
Vitwon <vitordeaguiar0@gmail.com>
2022-09-02 20:36:48 -0300

    Test Commit

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -42,7 +42,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.nav > :nth-child(2) > a').click()
     cy.get('#email').should('be.visible')
-    /* ==== End Cypress Studio ==== */
+    /* ===== End Cypress Studio ===== */
   })
 
 })
